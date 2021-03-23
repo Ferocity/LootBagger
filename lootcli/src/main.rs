d3f5a9b58da63b5cdf1ac5d195c0145bc161b8ef
Ferocity <christian.fries123@gmail.com>
2021-03-23 13:49:30 +0100

    add clients and daemon server

--- lootcli/src/main.rs
+++ lootcli/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use futures::prelude::*;
+use serde_json::json;
+use tokio::net::TcpStream;
+use tokio_serde::formats::*;
+use tokio_util::codec::{FramedWrite, LengthDelimitedCodec};
+
+#[tokio::main]
+pub async fn main() {
+    let socket = TcpStream::connect("127.0.0.1:8080").await.unwrap();
+
+    let length_delimited = FramedWrite::new(socket, LengthDelimitedCodec::new());
+    let mut serialized =
+        tokio_serde::SymmetricallyFramed::new(length_delimited, SymmetricalJson::default());
+
+    serialized
+        .send(json!({
+            "action": "test"
+        }))
+        .await
+        .unwrap()
 }
--- lootwatcher/src/main.rs
+++ lootwatcher/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use futures::prelude::*;
+use serde_json::json;
+use tokio::net::TcpStream;
+use tokio_serde::formats::*;
+use tokio_util::codec::{FramedWrite, LengthDelimitedCodec};
+use std::thread::sleep;
+use std::time::Duration;
+use futures::executor::block_on;
+
+
+#[tokio::main]
+pub async fn main() {
+    let mut socket = TcpStream::connect("127.0.0.1:8080").await.unwrap();
+
+    let length_delimited = FramedWrite::new(socket, LengthDelimitedCodec::new());
+    let mut serialized =
+        tokio_serde::SymmetricallyFramed::new(length_delimited, SymmetricalJson::default());
+
+    serialized
+        .send(json!(
+    {
+        "action": "update"
+    }
+    ))
+        .await
+        .unwrap();
+
 }
