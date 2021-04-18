b0e9232693fd83d67a974c53284358d68b834dc4
Christian Fries <christian.fries123@gmail.com>
2021-04-18 10:18:16 +0200

    remove tokio dependencies and init cli/watcher with basic argument parser

--- lootcli/src/main.rs
+++ lootcli/src/main.rs
@@ -1,21 +1,13 @@
-use futures::prelude::*;
-use serde_json::json;
-use tokio::net::TcpStream;
-use tokio_serde::formats::*;
-use tokio_util::codec::{FramedWrite, LengthDelimitedCodec};
+use simple_logger::SimpleLogger;
+use clap::{App, Arg, AppSettings};
 
-#[tokio::main]
-pub async fn main() {
-    let socket = TcpStream::connect("127.0.0.1:8080").await.unwrap();
 
-    let length_delimited = FramedWrite::new(socket, LengthDelimitedCodec::new());
-    let mut serialized =
-        tokio_serde::SymmetricallyFramed::new(length_delimited, SymmetricalJson::default());
+fn main() {
+    SimpleLogger::new().init().unwrap();
 
-    serialized
-        .send(json!({
-            "action": "test"
-        }))
-        .await
-        .unwrap()
+    let matches = App::new("LootCli")
+        .version("0.1")
+        .about("Command-Line tool to gather information during penetration tests and CTFs")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .get_matches();
 }
--- lootwatcher/src/main.rs
+++ lootwatcher/src/main.rs
@@ -1,37 +1,13 @@
-use futures::prelude::*;
-use serde_json::json;
-use tokio::net::TcpStream;
-use tokio_serde::formats::*;
-use tokio_util::codec::{FramedWrite, LengthDelimitedCodec, Framed};
-use std::thread::sleep;
-use futures::executor::block_on;
-use std::time::Duration;
-use tokio::time;
-use log::{info, trace, warn, error};
 use simple_logger::SimpleLogger;
+use clap::{App, Arg, AppSettings};
 
-#[tokio::main]
-pub async fn main() {
+
+fn main() {
     SimpleLogger::new().init().unwrap();
-    let mut socket = TcpStream::connect("127.0.0.1:8080").await.unwrap();
-    let length_delimited = Framed::new(socket, LengthDelimitedCodec::new());
-    let mut serialized =
-        tokio_serde::SymmetricallyFramed::new(length_delimited, SymmetricalJson::default());
-    let mut interval = time::interval(Duration::from_secs(1));
-    loop {
 
-        info!("Send update");
-        serialized.send(json!(
-        {
-            "action": "update"
-        }
-        )).await.unwrap();
-
-        info!("Receive");
-        while let Some(msg) = serialized.try_next().await.unwrap() {
-            info!("GOT: {:?} => {}", msg, msg["action"]);
-        }
-        info!("Receive");
-        interval.tick().await;
-    }
+    let matches = App::new("LootWatcher")
+        .version("0.1")
+        .about("Command-Line tool to display information useful for penetration testing or during CTFs")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .get_matches();
 }
