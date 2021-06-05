78f492e6e8ad62d27de78dd25e012d943fff0478
Christian Fries <christian.fries123@gmail.com>
2021-06-05 14:10:53 +0200

    Implemented create workspace file

--- core/src/workspace/file.rs
+++ core/src/workspace/file.rs
@@ -3,12 +3,36 @@ use std::path::Path;
 
 use log::*;
 
-use crate::workspace::fields::Workspace;
+use crate::workspace::fields::{Workspace, Settings, FolderConvention};
+use std::fs::File;
+use std::io::{Write, Error, ErrorKind};
 
+const WORKSPACE_FILE: &str = "workspace.yaml";
 
 impl Workspace {
-    pub fn create(filename: &Path) {
-        debug!("Create {}", filename.to_str().unwrap());
+    pub fn create(directory: &Path) -> std::io::Result<()> {
+        if !directory.is_dir() {
+            return Err(Error::last_os_error())
+        }
+
+        let file = directory.join(WORKSPACE_FILE);
+        if file.exists() {
+            return Err(Error::new(ErrorKind::AlreadyExists, "Workspace does already exist!"))
+        }
+
+        let mut fd = File::create(file)?;
+
+        let ws = Workspace {
+            name: "Test".to_string(),
+            url: "".to_string(),
+            settings: Settings {
+                preferred_folder_convention: FolderConvention::Name,
+                update_time: 100
+            }
+        };
+        let toml = toml::to_string(&ws).unwrap();
+        fd.write_all(toml.as_bytes())?;
+        Ok(())
     }
 
     pub fn load(filename: &Path) {
--- lootcli/src/main.rs
+++ lootcli/src/main.rs
@@ -1,6 +1,7 @@
 use simple_logger::SimpleLogger;
 use clap::{App, Arg, AppSettings};
-
+use core::workspace::fields::Workspace;
+use std::path::Path;
 
 fn main() {
     SimpleLogger::new().init().unwrap();
@@ -9,5 +10,23 @@ fn main() {
         .version("0.1")
         .about("Command-Line tool to gather information during penetration tests and CTFs")
         .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(App::new("workspace")
+            .setting(AppSettings::ArgRequiredElseHelp)
+            .arg(Arg::new("create")
+                .long("create")
+                .value_name("DIRECTORY")
+                .about("Create a new workspace in selected directory")))
         .get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("workspace") {
+        if let Some(workspace) = matches.value_of("create") {
+            let ws = Workspace::create(Path::new(workspace));
+            match ws {
+                Ok(_) => log::info!("Workspace created"),
+                Err(e) => log::error!("{}", e),
+            };
+        } else {
+            //println!()
+        }
+    }
 }
